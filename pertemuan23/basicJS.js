ad7e2e4495f503561a44622c57da7d27c07a97cb
nefotimontiling <s21610596@student.unklab.ac.id>
2020-11-09 20:52:07 +0800

    basic javascript

---
+++ pertemuan23/basicJS.js
@@ -0,0 +1,93 @@
+/*console.log("hello javascript");
+
+
+//variable
+
+//let & const
+
+//let bisah di ganti
+let umur = 25;
+let umur = 25;
+let sudahMenikah = true;
+let $nama  = "jhon";
+console.log(umur);
+console.log($nama);
+
+let alamat;
+console.log("alamat");
+
+// const menyimpam variable yang sudah tidak bisa rubah
+const tahunlahir = 2010;
+console.log(tahunlahir);
+/
+//operator
+// arithmetic operator (+,-,*,/)
+
+let angka1 = 39.37;
+let angka2 = 2.20;
+console.log ("angka1 * angka1") / 100
+
+let namaDepan = "Nefo";
+let namaBelakang = "Timontiling";
+let umur = 30;
+console.log(
+    "nama Saya " + namaDepan + " " + namaBelakang + ". Umur saya adalah " + umur + "tahun"
+)
+/
+
+//operator perbandingan
+
+let angka3 = 10;
+let angka4 = 10;
+
+let hasil = angka3 == angka4;
+console.log("hasil")
+
+//ternery operator
+/
+let angka = 5;
+let hasil = angka <= 0 ?  "Negatif" : "Positif";
+console.log("hasil");
+
+
+//operator logika
+
+let kondisi1 = true;
+let kondisi2 = false;
+console.log("kondisi1 && kondisi2");
+
+let angka = prompt ("masukan angka :  ");
+console.log("angka yang sudah di masukan adalah " + angka);
+
+//exsercise 7 
+// buatlah program javascript untuk menghitung BMI (body mass index)
+
+//rumus = berat (kg) (tinggi * tinggi)
+
+//gunakan prompt
+
+
+/
+//operator
+//arithmetic Operator (+,-,*,/)
+
+let angka1 = 17.0;
+let angka2 = 27.0;
+let hasil = (angka1 * angka1);
+
+let namadepan = "Nefo";
+let namaBelakang = "Timontiling";
+let umur = 30;
+console.log(
+    "nama Saya " + namaDepan  + " " +  namaBelakang   +  ". Umur saya adalah "  +  umur +  "tahun" )
+
+*/
+//BMI
+let badan1 = 17.0;
+let tinggi = 27.0;
+let BMI = (tinggi * tinggi);
+let hasil = (badan1 / BMI);
+
+let tinggi1 = prompt("masukan tinggi badan anda :");
+let badan2 = prompt("masukan berat badan anda :");
+console.log (" tinggi badan " * BMI / "berat badan" + badan1 + ". berat badan dan tinggi badan ideal "  + hasil);
